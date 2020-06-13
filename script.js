12b2945478d97368cd11401d845b269bf5680705
Rita Ziemann <rsziemann@gmail.com>
2020-06-13 16:55:19 -0400

    js

--- script.js
+++ script.js
@@ -1,45 +1,55 @@
-//list of all questions, choices, and answers
+//list of all questions, options, and answers
 
 var questions = [
   {
-    prompt: "commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts",
+    question: "Commonly used data types DO NOT include:",
+    option1: "strings",
+    option2: "boolean",
+    option3: "alert",
+    option4: "numbers",
+    answer: "3",
   },
   {
-    prompt: "The conditon in an if/ else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "quare brackets"],
-    answer: "parentheses",
+    question: "The conditon in an if/ else statement is enclosed within ____.",
+    option1: "quotes",
+    option2: "curly brackets",
+    option3: "parentheses",
+    option4: "quare brakcets",
+    answer: "3",
   },
   {
-    prompt: "Arrays in JavaScript can be used to store ____.",
-    choices: [
-      "numbers and strings",
-      "other arrays",
-      "booleans",
-      "all of the above",
-    ],
-    answer: "all of the above",
+    question: "Arrays in JavaScript can be used to store ____.",
+    option1: "numbers and strings",
+    option2: "other arrays",
+    option3: "booleans",
+    option4: "all of the above",
+    answer: "4",
   },
   {
-    prompt:
-      "string values must be enclosed wihtin ___when being assigned to variables.",
-    choices: ["commas", "curly brakcets", "quotes", "parentheses"],
-    answer: "quotes",
+    question:
+      "String values must be enclosed wihtin ___when being assigned to variables.",
+    option1: "commas",
+    option2: "curly brakcets",
+    option3: "quotes",
+    option4: "parentheses",
+    answer: "3",
   },
   {
-    prompt:
+    question:
       "A very useful too used during development and debuffing for printing content to the debugger is:",
-    choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
-    answer: "console.log",
+    option1: "JavaScript",
+    option2: "terminal/bash",
+    option3: "for loops",
+    option4: "console.log",
+    answer: "4",
   },
 ];
 
-var score = 0;
+//var score = 0;
 
 //for loop to move through array
 
-for(var i =0;) i < questions.length; i++){
+/*for(var i =0;) i < questions.length; i++){
     var response = window.prompt(questions[i].prompt);
     if(response === questions[i].answer){
         score++;
@@ -51,4 +61,4 @@ for(var i =0;) i < questions.length; i++){
 
 // alert at end of quiz
 
-alert("you got " + score + "/" + questions.length);
+alert("you got " + score + "/" + questions.length); */
