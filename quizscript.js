59ee127b2bfa281bce2a9586570f7cf97940326a
Rita Ziemann <rsziemann@gmail.com>
2020-06-13 17:57:59 -0400

    js errors corrected

--- quizscript.js
+++ quizscript.js
@@ -1,6 +1,6 @@
 var currentQuestion = 0;
 var score = 0;
-var toQuestions = questions.length;
+var totQuestions = questions.length;
 
 var container = document.getElementById("quizContainer");
 var questionEl = document.getElementById("question");
@@ -8,12 +8,12 @@ var opt1 = document.getElementById("opt1");
 var opt2 = document.getElementById("opt2");
 var opt3 = document.getElementById("opt3");
 var opt4 = document.getElementById("opt4");
-
 var nextButton = document.getElementById("nextButton");
+
 var resultCont = document.getElementById("result");
 
 function loadQuestion(questionIndex) {
-  var q = questions(questionIndex);
+  var q = questions[questionIndex];
   questionEl.textContent = questionIndex + 1 + "." + q.question;
   opt1.textContent = q.option1;
   opt2.textContent = q.option2;
