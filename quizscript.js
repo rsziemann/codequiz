24acab10d5feee70eed96c5affd176130a842557
Rita Ziemann <rsziemann@gmail.com>
2020-06-15 19:23:47 -0400

    edititng js

--- quizscript.js
+++ quizscript.js
@@ -1,3 +1,81 @@
+const startingMinutes = 2;
+
+let time = startingMinutes * 60;
+
+const countdownEl = document.getElementById("countdown");
+function updateCountdown() {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  countdownEl.innerHTML = `${minutes}: ${seconds}`;
+  time--;
+}
+
+//list of all questions, options, and answers, this is an array of objects
+
+var questions = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    option1: "strings",
+    option2: "boolean",
+    option3: "alert",
+    option4: "numbers",
+    answer: "3",
+  },
+  {
+    question: "The condition in an if/ else statement is enclosed within ____.",
+    option1: "quotes",
+    option2: "curly brackets",
+    option3: "parentheses",
+    option4: "quare brakcets",
+    answer: "3",
+  },
+  {
+    question: "Arrays in JavaScript can be used to store ____.",
+    option1: "numbers and strings",
+    option2: "other arrays",
+    option3: "booleans",
+    option4: "all of the above",
+    answer: "4",
+  },
+  {
+    question:
+      "String values must be enclosed wihtin ___when being assigned to variables.",
+    option1: "commas",
+    option2: "curly brakcets",
+    option3: "quotes",
+    option4: "parentheses",
+    answer: "3",
+  },
+  {
+    question:
+      "A very useful too used during development and debuffing for printing content to the debugger is:",
+    option1: "JavaScript",
+    option2: "terminal/bash",
+    option3: "for loops",
+    option4: "console.log",
+    answer: "4",
+  },
+];
+
+//var score = 0;
+
+//for loop to move through array
+
+/*for(var i =0;) i < questions.length; i++){
+    var response = window.prompt(questions[i].prompt);
+    if(response === questions[i].answer){
+        score++;
+        alert("correct!");
+    } else {
+        alert("wrong!"); 
+    }
+}
+
+// alert at end of quiz
+
+alert("you got " + score + "/" + questions.length); */
+
 var currentQuestion = 0;
 var score = 0;
 var totQuestions = questions.length;
@@ -14,6 +92,8 @@ var resultCont = document.getElementById("result");
 
 function loadQuestion(questionIndex) {
   var q = questions[questionIndex];
+  console.log(q);
+
   questionEl.textContent = questionIndex + 1 + "." + q.question;
   opt1.textContent = q.option1;
   opt2.textContent = q.option2;
@@ -23,7 +103,7 @@ function loadQuestion(questionIndex) {
 
 function loadNextQuestion() {
   var selectedOption = document.querySelector("input[type=radio]:checked");
-  if (!selectOption) {
+  if (!selectedOption) {
     alert("Please select your answer!");
     return;
   }
@@ -46,3 +126,7 @@ function loadNextQuestion() {
 }
 
 loadQuestion(currentQuestion);
+
+setInterval(function () {
+  updateCountdown();
+}, 1000);
